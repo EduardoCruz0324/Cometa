e2422e7d10c83777d6734df1f2b3dc311278d6fd
EduardoCruz0324 <eduardo_cruz_03@ciencias.unam.mx>
2026-03-03 17:59:03 -0600

    Actualización
    Error de dependecias.

---
+++ product/src/main/java/com/product/api/controller/CtrlCategory.java
@@ -0,0 +1,43 @@
+package com.product.api.controller;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.product.api.entity.Category;
+import com.product.api.service.SvcCategory;
+
+@RestController
+@RequestMapping("/category")
+public class CtrlCategory {
+
+	/*
+	@GetMapping
+	public List<Category> getCategories(){
+
+	    List<Category> categories = new ArrayList<Category>();
+
+	    categories.add(new Category(1,"lentes","LTS",1));
+	    
+	    Category relojes = new Category(2,"relojes","Rljs",1);
+	    categories.add(relojes);
+	    
+	    categories.add(new Category(3,"Zapatos","Charol",1));
+	    
+
+	    // agregar 3er categoria
+
+	    return categories;
+	}
+	*/
+	
+	@Autowired
+	SvcCategory svc;
+	
+	@GetMapping
+	public List<Category> getCategories(){
+		return svc.getCategories();
+	}
+	
+}
