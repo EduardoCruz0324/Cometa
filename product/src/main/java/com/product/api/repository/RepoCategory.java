fd3d845f919bad8bd42e1ed0e02c5a0d4ff04ca2
EduardoCruz0324 <eduardo_cruz_03@ciencias.unam.mx>
2026-03-05 07:54:01 -0600

    Arreglo De Error

--- product/src/main/java/com/product/api/repository/RepoCategory.java
+++ product/src/main/java/com/product/api/repository/RepoCategory.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import com.product.api.entity.Category;
 
 @Repository
-public interface RepoCategory {
+public interface RepoCategory extends JpaRepository<Category, Integer> {
 	
 	@Query(value="SELECT * FROM category ORDER BY category", nativeQuery=true)
 	List<Category> getCategories();
