c752915872b66e95d5f8d3c47f591daa40ab89e1
EduardoCruz0324 <eduardo_cruz_03@ciencias.unam.mx>
2026-03-11 10:35:16 -0600

    Practica4

--- product/src/main/java/com/product/api/controller/CtrlCategory.java
+++ product/src/main/java/com/product/api/controller/CtrlCategory.java
@@ -1,9 +1,12 @@
 package com.product.api.controller;
 
-import java.util.*;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.product.api.entity.Category;
 import com.product.api.service.SvcCategory;
@@ -36,7 +39,7 @@ public class CtrlCategory {
 	SvcCategory svc;
 	
 	@GetMapping
-	public List<Category> getCategories(){
+	public ResponseEntity<List<Category>> getCategories(){
 		return svc.getCategories();
 	}
 	
--- product/src/main/java/com/product/api/service/SvcCategoryImp.java
+++ product/src/main/java/com/product/api/service/SvcCategoryImp.java
@@ -3,20 +3,25 @@ package com.product.api.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import com.product.api.repository.RepoCategory;
 import com.product.api.entity.Category;
+import com.product.api.repository.RepoCategory;
+import com.product.exception.ApiException;
 
-@Service
-public class SvcCategoryImp implements SvcCategory {
-	
-	@Autowired
-	RepoCategory repo;
-	
-	@Override
-	public List<Category> getCategories(){
-		return repo.getCategories();
-	}
+public class SvcCategoryImp implements SvcCategory{
 
+    @Autowired
+    RepoCategory repo;
+
+    @Override
+    public ResponseEntity<List<Category>>getCategories(){
+        try{
+            return new ResponseEntity<>(repo.getCategories(),HttpStatus.OK);            
+        }catch(DataAccessException e){
+            throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR, "Error al consultar la base de datos. ");
+        }
+    }
 }
